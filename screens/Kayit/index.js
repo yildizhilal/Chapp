a7fb808822ac39f7bd83a20bcd92e39be0a5a3cd
tokyaycanan <74538783+tokyaycanan@users.noreply.github.com>
2020-11-18 18:17:27 +0300

    Update index.js

--- screens/Kayit/index.js
+++ screens/Kayit/index.js
@@ -1,64 +1,75 @@
 import React, { useState }  from 'react';
 import {View, Text, StyleSheet,Button,TextInput, ImageBackground,TouchableOpacity,Picker } from 'react-native';
 
-
 const Kayit = props => {
 
   const {navigation} = props;
   state={
-    cinsiyet:"",
-    kilo:"",
-    boy:"",
-    yas:"",
-    hedef:"",
+   kullanıcıAdı:"",
+   isim:"",
+   soyisim:"",
+   email:"",
+   password:"",
+
   }
-  const [selectedValue, setSelectedValue] = useState("Cinsiyetiniz..");
+  const [selectedValue, setSelectedValue] = useState("seçim yapınız");
   return (
 
     <ImageBackground style={{flex: 1, opacity: 0.9,}} source={{uri: 'https://cdn.pixabay.com/photo/2019/05/28/10/05/rock-4234793_960_720.jpg'}}>
 <View style={styles.container}>
     <Text style={styles.logo}>Bazı bilgilere ihtiyacımız var..:)</Text>
+   
+    <View style={styles.inputView} >
+      <TextInput  
+        style={styles.inputText}
+        placeholder="Kullanıcı adı:" 
+        placeholderTextColor="#003f5c"
+        onChangeText={text => this.setState({kullanıcıAdı:text})}/>
+    </View>
+    <View style={styles.inputView} >
+      <TextInput  
+        secureTextEntry
+        style={styles.inputText}
+        placeholder="adınz:" 
+        placeholderTextColor="#003f5c"
+        onChangeText={text => this.setState({isim:text})}/>
+    </View>
+    <View style={styles.inputView} >
+      <TextInput  
+        style={styles.inputText}
+        placeholder="Soyadınız:" 
+        placeholderTextColor="#003f5c"
+        onChangeText={text => this.setState({soyisim:text})}/>
+    </View>
+    <View style={styles.inputView} >
+      <TextInput  
+        style={styles.inputText}
+        placeholder="E-mail:" 
+        placeholderTextColor="#003f5c"
+        onChangeText={text => this.setState({email:text})}/>
+    </View>
+    <View style={styles.inputView} >
+      <TextInput  
+        style={styles.inputText}
+        placeholder="password:" 
+        placeholderTextColor="#003f5c"
+        onChangeText={text => this.setState({password:text})}/>
+    </View>
     <Picker
         selectedValue={selectedValue}
         style={styles.pickerStyle}
         onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
         
       >
-        <Picker.Item label="Cinsiyetiniz.." value=" " />
-        <Picker.Item label="kadin" value="kadin" />
-        <Picker.Item label="erkek" value="erkek" />
+        <Picker.Item label="seçim yapınız" value=" " />
+        <Picker.Item label="Kilomu Koru" value="koru" />
+        <Picker.Item label="Kilo Ver" value="ver" />
+        <Picker.Item label="Kilo al" value="al" />
       </Picker>
-    <View style={styles.inputView} >
-      <TextInput  
-        style={styles.inputText}
-        placeholder="Kilonuz:" 
-        placeholderTextColor="#003f5c"
-        onChangeText={text => this.setState({kilo:text})}/>
-    </View>
-    <View style={styles.inputView} >
-      <TextInput  
-        secureTextEntry
-        style={styles.inputText}
-        placeholder="Boyunuz:" 
-        placeholderTextColor="#003f5c"
-        onChangeText={text => this.setState({boy:text})}/>
-    </View>
-    <View style={styles.inputView} >
-      <TextInput  
-        style={styles.inputText}
-        placeholder="Yaşınız:" 
-        placeholderTextColor="#003f5c"
-        onChangeText={text => this.setState({yas:text})}/>
-    </View>
-    <View style={styles.inputView} >
-      <TextInput  
-        style={styles.inputText}
-        placeholder="Hedef Kilonuz:" 
-        placeholderTextColor="#003f5c"
-        onChangeText={text => this.setState({hedef:text})}/>
-    </View>
- 
-    <TouchableOpacity style={styles.devamBtn}>
+    <TouchableOpacity style={styles.devamBtn}
+     onPress={() => navigation.navigate('KayitNext')}
+    >
+     
       <Text style={styles.devamText}>DEVAM</Text>
     </TouchableOpacity>
    
@@ -132,5 +143,6 @@ const Kayit = props => {
   
   });
   
+  
+  export default Kayit;
 
-export default Kayit;

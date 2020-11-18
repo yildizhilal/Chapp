9ff02c9a28208cbf6815b527cf06b79974cb24b2
tokyaycanan <74538783+tokyaycanan@users.noreply.github.com>
2020-11-18 19:17:45 +0300

    Update index.js

--- screens/StartPage/index.js
+++ screens/StartPage/index.js
@@ -1,60 +1,86 @@
-import React from 'react';
-import {View, Text, StyleSheet,Button} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, ImageBackground} from 'react-native';
 
 const StartPage = props => {
 
-    const {navigation} = props;
+  const {navigation} = props;
 
-  return (
-    <View style={styles.container}>
-        <Text style={styles.text}>CHAPP'E Hoşgeldiniz</Text>
-      <View style={styles.buttonContainer}>
-        <View style={styles.kayitText}>
-      <Text 
-           onPress={() => navigation.navigate('Kayit')}
-          style={styles.textButton}>Kayıt Ol</Text>
-      </View>
-      <View style={styles.kayitText} >
-      <Text
-          onPress={() => navigation.navigate('Login')} 
-          style={styles.textButton}>Giriş Yap</Text>
-      </View>
-      </View>
+return (
+  <View style={styles.container}>
+     <ImageBackground style={{flex: 1, opacity: 0.87,}} source={{uri: 'https://cdn.pixabay.com/photo/2016/03/01/08/06/harmony-1229886_960_720.jpg'}}>
+      <Text style={styles.text}>HOŞGELDİN</Text>
+      <Text style={styles.textNext}>Hadi başlayalım...</Text>
+    <View style={styles.buttonContainer}>
+      <View style={styles.kayitText}>
+    <Text 
+         onPress={() => navigation.navigate('Kayit')}
+        style={styles.textButton}>KAYIT OL</Text>
     </View>
-  );
+    <View style={styles.kayitText} >
+    <Text
+        onPress={() => navigation.navigate('Login')} 
+        style={styles.textButton}>GİRİŞ YAP</Text>
+    </View>
+    </View>
+    </ImageBackground>
+  </View>
+  
+);
 };
 
 const styles = StyleSheet.create({
-    container:{
-      flex: 1, 
-      justifyContent: 'center', 
-      alignItems: 'center',
-      backgroundColor: '#FFB74D'
-    },
-    text:{
-      height:'50%',
-        fontWeight: 'bold',
-        fontSize: 40,
-        textAlign: "center",
-        fontStyle: 'italic',
-    },
-    buttonContainer:{
-      justifyContent:'space-between',
-      width:'50%',
-      height:'25%',
-    },
-    kayitText:{
-     
-      backgroundColor: "#FFCC80",
-      borderWidth: 1,
-      borderRadius: 20,
-      justifyContent:"space-around",
-      fontSize: 20,
-    },
-    textButton:{
-      fontSize:20,
+  container:{
+    flex: 1, 
+    
+  
+   
+  },
+  
+
+  text:{
+    marginTop: 20,
+    height:'43%',
+    color:"#022040",
+      fontWeight: 'bold',
+      fontSize: 65,
       textAlign: "center",
-    },
+      fontStyle: 'italic',
+      
+  },
+
+  textNext:{
+    height:'15%',
+    color:"#022040",
+      fontWeight: 'bold',
+      fontSize: 35,
+      textAlign: "center",
+      fontStyle: 'italic',
+  },
+  buttonContainer:{
+    justifyContent:'space-between',
+    width:'80%',
+    height:'20%',
+    alignSelf: 'center',
+    
+    
+    
+  },
+  kayitText:{
+   
+    backgroundColor: 'rgba(0,0,0,0.65)',
+    
+    borderWidth: 2,
+    borderRadius: 50,
+    justifyContent:"space-around",
+    fontSize: 50,
+    opacity:0.8
+  },
+  textButton:{
+    fontSize:30,
+    textAlign: "center",
+    color:"#f7f7f7",
+   
+  },
 
 
 });

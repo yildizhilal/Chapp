8613800b4666abcef8034a290d399806af089a6b
Canan Tokyay <tokyaycanan@gmail.com>
2020-12-31 21:06:14 +0300

    update su
    arayüz tasarlandı

--- screens/SuMiktar/index.js
+++ screens/SuMiktar/index.js
@@ -1,10 +1,9 @@
 import React, {useEffect, useState} from "react";
-import { View, StyleSheet, Text,TouchableOpacity,FlatList} from "react-native";
+import { View, StyleSheet,Image,ImageBackground, Text,TouchableOpacity} from "react-native";
 
 import Firebase from "../../config/Firebase";
 import moment from "moment";
-import { AntDesign } from '@expo/vector-icons'; 
-import { Entypo } from '@expo/vector-icons'; 
+
 
 
 //disable yellow warnings on EXPO client!
@@ -34,13 +33,30 @@ const SuMiktar = props => {
     return () => subscriber();
   }, []);
 
+  const [count, setCount] = useState(0);
 
-
+  function  _kontrol(n) {
+    if (count==n){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+  function  _kontrol2(n) {
+    if (count<n){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 
 
 const[su,setsu]=useState(250)
 
-  su_ekle=()=>{
+   su_ekle=()=>{
+    setCount(count+ 1);
     setsu((su+250))
     var SU= Firebase.firestore().collection("Users").doc(user).collection("GunlukTakip").doc(date)
      var setWithMerge = SU.set({
@@ -52,17 +68,196 @@ const[su,setsu]=useState(250)
 
   return (
     <View style={styles.center}>
+       <ImageBackground style={{ flex:1,}} source={{uri: 'https://cdn.pixabay.com/photo/2014/11/25/16/32/drop-of-water-545377_960_720.jpg'}}>
+
       <View style={styles.title}>
-          <Text style={{fontWeight:"700",fontSize:40, color:"white"}}>This is the SuMiktar screen</Text>
+          <Text style={{fontWeight:"700",fontSize:40, color:"white" ,marginTop:"15%"}}>  SU HAYATTIR!</Text>
       </View>
       <View style={styles.miktar}>
         <Text style={styles.miktartext}>Bugün Toplam {water} Mililitre Su İçtin</Text>
         <Text style={styles.miktartext}>Tebrikler Böyle Devam Et!</Text>
       </View>
-      <TouchableOpacity style={styles.addList} onPress={()=>su_ekle()}>
-            <AntDesign name="plus" size={16} color="white" />
-      </TouchableOpacity>
-   
+  
+       <View style={styles.controlSpace}>
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(0)}
+          style={ _kontrol2(1) ? styles.btn2 : styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(1)}
+          style={ _kontrol2(2) ? styles.btn2: styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+     
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(2)}
+          style={ _kontrol2(3) ? styles.btn2: styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+     
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(3)}
+          style={ _kontrol2(4) ? styles.btn2: styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(4)}
+          style={ _kontrol2(5) ? styles.btn2: styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(5)}
+          style={ _kontrol2(6) ? styles.btn2: styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.controlSpace}>
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(6)}
+          style={ _kontrol2(7) ? styles.btn2 : styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(7)}
+          style={ _kontrol2(8) ? styles.btn2: styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(8)}
+          style={ _kontrol2(9) ? styles.btn2: styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+       
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(9)}
+          style={ _kontrol2(10) ? styles.btn2: styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(10)}
+          style={ _kontrol2(11) ? styles.btn2: styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+        <TouchableOpacity 
+          onPress={()=>su_ekle()}
+          disabled={_kontrol(11)}
+          style={ _kontrol2(12) ? styles.btn2: styles.btn1}
+          >
+              <Image
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2014/04/03/10/31/glass-310759_960_720.png',
+                }}
+                style={styles.buttonImageIconStyle}
+              />
+              <Text>+250ml</Text>
+        </TouchableOpacity>
+
+        </View>
+        <View style={styles.txt}>
+        <Text style={styles.txtson}>Bir günde en az 2 litre su içmelisin!</Text>
+        </View>
+       </ImageBackground>
     </View>
   );
 };
@@ -70,44 +265,90 @@ const[su,setsu]=useState(250)
 const styles = StyleSheet.create({
   center: {
     flex: 1,
-    backgroundColor:"#81D4FA"
+
   },
   title:{
     padding:15,
     alignItems:"center",
-    borderBottomWidth:5,
+    borderBottomWidth:0,
     borderBottomColor:"white"
   },
-  addList: {
-    marginTop:"10%",
-    marginLeft:"40%",
-    width:"20%",
-    borderWidth: 4,
-    borderColor:"#B2EBF2",
-    borderRadius: 4,
-    padding: 16,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+
   miktar:{
     
     alignItems: "center",
     justifyContent: "center",
-    marginTop:"5%"
+    marginTop:"2%",
+    marginBottom:"2%",
+    width:"90%",
+    backgroundColor:"#e3dcce",
+    borderRadius:25,
+    height: "15%",
+    padding:"7%",
+    alignSelf: 'center',
+    borderWidth: 1,
+    borderColor: "#844b15",
     
   },
   miktartext:{
-    fontSize:20,
+    fontSize:21,
     alignItems: "center",
     justifyContent: "center",
-    color:"white"
+    color:"black",
+    marginBottom:"4%",
     
   },
-  drop:{
-    marginTop:"10%",
-    flexDirection:"row"
-
-  }
+  txtson:{
+    fontSize:25,
+    alignItems: "center",
+    justifyContent: "center",
+    color:"white",
+  
+  },
+  txt:{
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop:"0%",
+  },
+  btn1: {
+  
+    alignItems: 'center',
+    backgroundColor: '#485a96',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: "30%",
+    width: "15%",
+    borderRadius: 5,
+    margin: "0.5%",
+    opacity:1,
+  },
+  buttonImageIconStyle: {
+    padding: 15,
+    margin: "15%",
+    height: "75%",
+    width: "15%",
+    resizeMode: 'stretch',
+    alignItems: 'center',
+  },
+  btn2:{
+ 
+    alignItems: 'center',
+    backgroundColor: '#00e6e6',
+    borderWidth: 0.5,
+    borderColor: '#fff',
+    height: "30%",
+    width: "15%",
+    borderRadius: 5,
+    margin: "0.5%",
+    opacity:0.3,
+  },
+  controlSpace: {
+    flexDirection: 'row',
+    justifyContent:"center",
+    marginTop: "1%",
+    
+  
+  },
 });
 
 export default SuMiktar;
